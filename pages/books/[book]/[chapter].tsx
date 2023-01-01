c83e740bf1aefc6bd4854e5bebd9142f109ded79
nt102k <58685832+nt102k@users.noreply.github.com>
2023-01-01 14:22:37 +0700

    Change Color

--- pages/books/[book]/[chapter].tsx
+++ pages/books/[book]/[chapter].tsx
@@ -3,7 +3,7 @@ import Router from "next/router"
 export default function Chapter(){
     return (
         <>
-            <button onClick={() => Router.back()} className="font-bold text-sm px-4 py-2 bg-gray-600 hover:bg-gray-500 rounded-sm shadow-sm">Go back</button>
+            <button onClick={() => Router.back()} className="font-bold text-sm px-4 py-2 bg-gray-800 hover:bg-gray-900 text-white rounded-sm shadow-sm">Go back</button>
             <h1 className="text-lg font-bold">Cuốn sách về những người hãy ho</h1>
         </>
     )
