d02d159df03fda82a1ee28c8e62c7435638fa76a
nt102k <58685832+nt102k@users.noreply.github.com>
2023-01-01 14:21:03 +0700

    Chỉnh sửa phẩm xem chương

--- pages/books/[book]/[chapter].tsx
+++ pages/books/[book]/[chapter].tsx
@@ -3,7 +3,7 @@ import Router from "next/router"
 export default function Chapter(){
     return (
         <>
-            <p onClick={() => Router.back()} className="font-bold text-sm">Go back</p>
+            <button onClick={() => Router.back()} className="font-bold text-sm px-4 py-2 bg-gray-600 hover:bg-gray-500 rounded-sm shadow-sm">Go back</button>
             <h1 className="text-lg font-bold">Cuốn sách về những người hãy ho</h1>
         </>
     )
