3092e28d6f0ab6ac6d9c00fa6374e376a1f892e8
nt102k <58685832+nt102k@users.noreply.github.com>
2023-01-01 14:38:16 +0700

    Cập nhật head

--- pages/books/[book]/[chapter].tsx
+++ pages/books/[book]/[chapter].tsx
@@ -1,10 +1,19 @@
 import Router from "next/router"
+import Head from "next/head"
 
 export default function Chapter(){
     return (
         <>
-            <button onClick={() => Router.back()} className="font-bold text-sm px-4 py-2 bg-gray-800 hover:bg-gray-900 text-white rounded-sm shadow-sm">Go back</button>
-            <h1 className="text-lg font-bold capitalize">Cuốn sách về những người hãy ho</h1>
+            <Head>
+                <title>Chương Truyện | Đọc Truyện App</title>
+                <meta name="description" content="Chương truyện là đây" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main>
+                <button onClick={() => Router.back()} className="font-bold text-sm px-4 py-2 bg-gray-800 hover:bg-gray-900 text-white rounded-sm shadow-sm">Go back</button>
+                <h1 className="text-lg font-bold capitalize">Cuốn sách về những người hãy ho</h1>
+            </main>
         </>
     )
 }
